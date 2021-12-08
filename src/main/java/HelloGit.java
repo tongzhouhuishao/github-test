7d52cd9c33d32af6da306ac3f91b149d7afee93d
huishao <huishao@qq.com>
2021-12-08 22:20:59 +0800

    push commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
         System.out.println("我是存储在git本地仓库的程序");
         System.out.println("又新添加了一些");
         System.out.println("我是新的hot-fix分支修改的");
+        System.out.println("push-test");
     }
 }
