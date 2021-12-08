c2c5c00f6f3128af44286cbda6d90b05e5cd5fe7
huishao <huishao@qq.com>
2021-12-08 22:04:36 +0800

    hot-fix first commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("我是存储在git本地仓库的程序");
         System.out.println("又新添加了一些");
+        System.out.println("我是新的hot-fix分支修改的");
     }
 }
